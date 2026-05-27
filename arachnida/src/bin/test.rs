ff8f797a6b8aed093ca5542241bd349bad92a9d8
chloezhu010 <chloe.zhu.x@gmail.com>
2026-05-27 23:53:45 +0200

    feat: add url crate and prototype url parsing

--- arachnida/src/bin/test.rs
+++ arachnida/src/bin/test.rs
@@ -1,5 +1,7 @@
 // use clap::Parser;
-use scraper:: {Html, Selector};
+// use scraper:: {Html, Selector};
+use url::Url;
+
 
 
 // /// Simple program to greet a person
@@ -38,30 +40,49 @@ use scraper:: {Html, Selector};
 //     Ok(())
 // }
 
-fn main() {
-    let html = r#"
-          <html><body>
-              <img src="cat.jpg">
-              <img src="/img/dog.png" alt="dog">
-              <img src="https://cdn.example.com/bird.gif">
-              <a href="page2.html"><img src="../mouse.bmp"></a>
-              <img>   <!-- no src at all -->
-          </body></html>
-    "#;
+// fn main() {
+//     let html = r#"
+//           <html><body>
+//               <img src="cat.jpg">
+//               <img src="/img/dog.png" alt="dog">
+//               <img src="https://cdn.example.com/bird.gif">
+//               <a href="page2.html"><img src="../mouse.bmp"></a>
+//               <img>   <!-- no src at all -->
+//           </body></html>
+//     "#;
 
-    // let html = r#"
-    //     <!DOCTYPE html>
-    //     <meta charset="utf-8">
-    //     <title>Hello, world!</title>
-    //     <h1 class="foo">Hello, <i>world!</i></h1>
-    // "#;
+//     // let html = r#"
+//     //     <!DOCTYPE html>
+//     //     <meta charset="utf-8">
+//     //     <title>Hello, world!</title>
+//     //     <h1 class="foo">Hello, <i>world!</i></h1>
+//     // "#;
     
-    let doc = Html::parse_document(html);
-    // println!("Parsed HTML document: {:#?}", doc);
-    let selector = Selector::parse("img").unwrap();
-    for element in doc.select(&selector) {
-        let src = element.value().attr("src");
-        println!("found img, src = {:?}", src);
-    }
+//     let doc = Html::parse_document(html);
+//     // println!("Parsed HTML document: {:#?}", doc);
+//     let selector = Selector::parse("img").unwrap();
+//     for element in doc.select(&selector) {
+//         let src = element.value().attr("src");
+//         println!("src: {:?}", src);
+//         println!("raw value: {:?}\n", element.value());
+//     }
 
+// }
+
+fn main() {
+    // let this_document = Url::parse("http://servo.github.io/rust-url/url/index.html").unwrap();
+    // let css_url = this_document.join("../main.css").unwrap();
+    // println!("CSS URL: {}", css_url);
+
+    let base = Url::parse("https://example.com/blog/post/").unwrap();
+    println!("Base URL: {}", base);
+
+    println!("\n--- parts of the URL ---");
+    println!("Scheme: {}", base.scheme());
+    println!("Host: {:?}", base.host_str());
+    println!("Port: {:?}", base.port());
+    println!("Path: {}", base.path());
+    println!("Query: {:?}", base.query());
+
+    println!("\n--- base.join(src) for each case ---");
 }
